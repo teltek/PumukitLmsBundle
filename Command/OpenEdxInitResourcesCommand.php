091a31462dfa2e197dd788acebb4f245a2029e8c
mrey <mrey@teltek.es>
2018-03-09 14:16:15 +0100

    TTK-17292: Update command for enable copy id from manager - add version templates

--- Command/OpenEdxInitResourcesCommand.php
+++ Command/OpenEdxInitResourcesCommand.php
@@ -4,6 +4,7 @@ namespace Pumukit\OpenEdxBundle\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -16,6 +17,7 @@ class OpenEdxInitResourcesCommand extends ContainerAwareCommand
     {
         $this
           ->setName('openedx:init:resources')
+          ->addArgument('version', InputArgument::REQUIRED, 'Select the version of PuMuKIT to override PumukitNewAdminBundle list template: 2.3.x, 2.4.x')
           ->setDescription('Initialize the resources necessary to add a button to insert a VoD into OpenEdx/Moodle')
           ->setHelp(<<<EOT
 Initialize the resources necessary to add a button to insert a VoD into OpenEdx/Moodle. It copies the global resources from Resources/data/override bundle dir to
@@ -30,10 +32,12 @@ EOT
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $version = $input->getArgument('version');
+
         $finder = new Finder();
         $fs = new Filesystem();
 
-        $fromDir = realpath(__DIR__.'/../'.self::OVERRIDE_DATA_DIR);
+        $fromDir = realpath(__DIR__.'/../'.self::OVERRIDE_DATA_DIR.'/'.$version);
         $toDir = $this->getContainer()->get('kernel')->getRootDir().'/Resources';
 
         $output->writeln('Coping resources from <info>'.$fromDir.'</info> to <info>'.$toDir.'</info>:');
