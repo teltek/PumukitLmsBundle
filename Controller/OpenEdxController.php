77b7954b6fd8bd3094edfda772ae0a59ccba0793
rubenrua <rubenrua@teltek.es>
2018-10-23 15:15:35 +0200

    TTK-19146 404 if object is blocked
    See TTK-19146#note-15

--- Controller/OpenEdxController.php
+++ Controller/OpenEdxController.php
@@ -76,7 +76,7 @@ class OpenEdxController extends SSOController
 
         $job = $jobRepo->findOneBy(array('mm_id' => $id, 'profile' => array('$in' => $profileNames)));
 
-        if ($multimediaObject && !$multimediaObject->isBlocked()) {
+        if ($multimediaObject && $multimediaObject->isPublished()) {
             if ($multimediaObject->containsAnyTagWithCodes(['PUCHWEBTV', 'PUCHLMS'])) {
                 if (!$job || ($job && Job::STATUS_FINISHED !== $job->getStatus())) {
                     return new Response($this->renderView('PumukitLmsBundle:OpenEdx:400job.html.twig', array('id' => $id, 'job' => $job, 'email' => $contactEmail, 'openedx_locale' => $locale)), 400);
