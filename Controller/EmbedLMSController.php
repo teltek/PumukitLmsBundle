82998b76aabcafa4f8d407d90ab069c013fff1fa
Alba <albalago.18@gmail.com>
2025-08-05 12:32:25 +0200

    feat(27775): Add property to added multimediaObject in lms

---
+++ Controller/EmbedLMSController.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pumukit\LmsBundle\Controller;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmbedLMSController extends AbstractController
+{
+    private $documentManager;
+
+    public function __construct(DocumentManager $documentManager)
+    {
+        $this->documentManager = $documentManager;
+    }
+
+    /**
+     * @Route("/lms/embed", name="pumukit_lms_embed", methods={"POST"})
+     */
+    public function embedLms(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $mmId = $data['mmId'] ?? null;
+
+        if (!$mmId) {
+            return new JsonResponse(['error' => 'Missing mmId'], 400);
+        }
+
+        $multimediaObject = $this->documentManager->getRepository(MultimediaObject::class)->findOneBy([
+            '_id' => $mmId,
+        ]);
+
+        if (!$multimediaObject) {
+            return new JsonResponse(['error' => 'MultimediaObject not found'], 404);
+        }
+
+        $multimediaObject->setProperty('embedded_in_lms', date('Y'));
+        $this->documentManager->flush();
+
+        return new JsonResponse(['status' => 'ok', 'mmId' => $mmId]);
+    }
+}
